624024e81d581a0b4c55923c68ee627bedf10190
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-01-23 23:50:51 -0700

    added pseudo code to js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// create one function for each operator
+// ----- using Object to store operators ----
+// create function operate - takes 3 par - 2 numbers & operator
+// populate display when click/press key
+// save display value
+
