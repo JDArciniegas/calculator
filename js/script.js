2d506fb2866fa99ea19e101a013b105eb16d4547
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-01-24 23:02:38 -0700

    complete basic styling, starting with display function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+// --------------------variables--------------------
+const numberInput = document.querySelector('#numberInput');
+
+input.oninput = function () {
+  result.innerHTML = input.value;
+};
 // ----- using Object to store operators ----
 let operation = {
   '+': function (a, b) { return a + b },
@@ -5,12 +11,17 @@ let operation = {
   '*': function (a, b) { return a * b },
   '/': function (a, b) { return a / b },
 }
+// --------------------events--------------------
+
+// -----------------functions---------------------
 // create one function for each operator
 // create function operate - takes 3 par - 2 numbers & operator
 function operate(val1, val2, operator){
   return operator(val1, val2);
 }
 
+
+// -----------------test----------------
 console.log(operate(2, 3, operation['+']));
 console.log(operate(5, 5, operation['*']));
 console.log(operate(100, 10, operation['-']));
