43ead7b6a75c8b059287943c942036d7466bd8a5
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-01-24 23:45:46 -0700

    keydown on window

--- js/script.js
+++ js/script.js
@@ -4,14 +4,13 @@ const lastOperation = document.querySelector('#last-operation');
 
 // ----- using Object to store operators ----
 // --------------------events--------------------
+window.addEventListener('keydown', (e) => {
+  console.log(e.key);
+})
 // -----------------functions---------------------
 // create one function for each operator
 // create function operate - takes 3 par - 2 numbers & operator
 
 // -----------------test----------------
-console.log(operate(2, 3, operation['+']));
-console.log(operate(5, 5, operation['*']));
-console.log(operate(100, 10, operation['-']));
-console.log(operate(9, 3, operation['/']));
 // populate display when click/press key
 // save display value
