968aacd111bf8c046e70e59caf84a61b93916fc2
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-01-24 22:00:32 -0700

    created operator Object

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-// create one function for each operator
 // ----- using Object to store operators ----
+let operation = {
+  '+': function (a, b) { return a + b },
+  '-': function (a, b) { return a - b },
+  '*': function (a, b) { return a * b },
+  '/': function (a, b) { return a / b },
+}
+// create one function for each operator
+// function operate(val1, val2, operator){
+
+// }
+
+console.log(operation['+'](2, 3));
+console.log(operation['-'](2, 3));
+console.log(operation['*'](2, 3));
+console.log(operation['/'](2, 3));
 // create function operate - takes 3 par - 2 numbers & operator
 // populate display when click/press key
 // save display value
-
