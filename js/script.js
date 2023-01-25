15dff3b8939eb9b499f4cb35213dc1d36196f3a5
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-01-24 22:08:21 -0700

    created function with 2 numbers  operator

--- js/script.js
+++ js/script.js
@@ -6,14 +6,14 @@ let operation = {
   '/': function (a, b) { return a / b },
 }
 // create one function for each operator
-// function operate(val1, val2, operator){
+function operate(val1, val2, operator){
+  return operator(val1, val2);
+}
 
-// }
-
-console.log(operation['+'](2, 3));
-console.log(operation['-'](2, 3));
-console.log(operation['*'](2, 3));
-console.log(operation['/'](2, 3));
+console.log(operate(2, 3, operation['+']));
+console.log(operate(5, 5, operation['*']));
+console.log(operate(100, 10, operation['-']));
+console.log(operate(9, 3, operation['/']));
 // create function operate - takes 3 par - 2 numbers & operator
 // populate display when click/press key
 // save display value
