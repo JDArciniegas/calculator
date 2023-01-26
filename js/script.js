e3f532b3cb4f1c4623145b33eead220babb96f90
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-01-25 23:07:48 -0700

    added keydown event

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // --------------------variables--------------------
-// const result = document.querySelector('#result');
-// const lastOperation = document.querySelector('#last-operation');
+const result = document.querySelector('#result');
+const lastOperation = document.querySelector('#last-operation');
 
 // ----- using Object to store operators ----
 const add = (a, b) => a + b;
@@ -8,8 +8,12 @@ const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-function executeOperation(value1, value2, operator){
-  switch(operator){
+// -----------------functions---------------------
+// create one function for each operator
+// create function operate - takes 3 par - 2 numbers & operator
+
+function executeOperation(value1, value2, operator) {
+  switch (operator) {
     case '+':
       return add(value1, value2);
       break;
@@ -25,14 +29,12 @@ function executeOperation(value1, value2, operator){
   }
 }
 
+function updateResultDisplay(result, display){
+  display.append(result);
+}
 // --------------------events--------------------
-// window.addEventListener('keydown', (e) => {
-//   console.log(e.key);
-// })
-// -----------------functions---------------------
-// create one function for each operator
-// create function operate - takes 3 par - 2 numbers & operator
+window.addEventListener('keydown', (e) => {
+  updateResultDisplay(e.key, result)
+  console.log(e.key);
+})
 
-// -----------------test----------------
-// populate display when click/press key
-// save display value
